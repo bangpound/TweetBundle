2a93beaedc78fba6b9aa2d386c738f13cbe55da1
Benjamin Doherty <bjd@bangpound.org>
2013-07-08 21:33:13 -0500

    disable pager when there are no filter conditions on twitter list.

--- Datagrid/Pager.php
+++ Datagrid/Pager.php
@@ -7,4 +7,33 @@ use Doctrine\ORM\Query;
 
 class Pager extends BasePager
 {
+
+    public function init()
+    {
+        $where = $this->getQuery()->getQueryBuilder()->getDQLPart('where');
+        if (!empty($where))
+        {
+            parent::init();
+        }
+        else
+        {
+            $this->resetIterator();
+
+            $this->getQuery()->setFirstResult(null);
+            $this->getQuery()->setMaxResults(null);
+
+            if (count($this->getParameters()) > 0) {
+                $this->getQuery()->setParameters($this->getParameters());
+            }
+
+            if (0 == $this->getPage() || 0 == $this->getMaxPerPage()) {
+                $this->setLastPage(0);
+            } else {
+                $offset = ($this->getPage() - 1) * $this->getMaxPerPage();
+
+                $this->getQuery()->setFirstResult($offset);
+                $this->getQuery()->setMaxResults($this->getMaxPerPage());
+            }
+        }
+    }
 }
