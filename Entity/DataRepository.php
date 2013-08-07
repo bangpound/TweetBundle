1a9165ae5d1b5e7368ab2459a7e9a68785d198aa
Benjamin Doherty <bjd@bangpound.org>
2013-08-07 18:03:07 -0500

    remove nonsense locking.

--- Entity/DataRepository.php
+++ Entity/DataRepository.php
@@ -2,23 +2,10 @@
 
 namespace Bangpound\Twitter\DataBundle\Entity;
 
-use Doctrine\DBAL\LockMode;
 use Doctrine\ORM\EntityRepository;
 
 class DataRepository extends EntityRepository
 {
 
-    /**
-     * Finds a single entity by a set of criteria and lock it.
-     *
-     * @param array $criteria
-     * @param array|null $orderBy
-     * @return object
-     */
-    public function findAndLockOneBy(array $criteria, array $orderBy = null)
-    {
-        $persister = $this->_em->getUnitOfWork()->getEntityPersister($this->_entityName);
 
-        return $persister->load($criteria, null, null, array(), LockMode::PESSIMISTIC_WRITE, 1, $orderBy);
-    }
 }
